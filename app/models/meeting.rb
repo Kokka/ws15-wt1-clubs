c584019af2c1765066e5dafdfa4beefc452e75ff
Schollone <s0547433@htw-berlin.de>
2015-11-11 20:43:25 +0100

    added dependency to meeting.rb (destroys invites, when meeting is destroyed)

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,4 @@
 class Meeting < ActiveRecord::Base
   belongs_to :group
-  has_many :invites
+  has_many :invites, dependent: :destroy
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,4 @@
 class Member < ActiveRecord::Base
-	has_and_belongs_to_many :group
+	has_and_belongs_to_many :groups
 	has_many :invites
 end
