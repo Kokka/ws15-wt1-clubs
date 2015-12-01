23ffbaffa8a2c724e7639b5f591a4034a78bf75d
rennersebastian <renner.sebastian@gmail.com>
2015-12-01 16:31:35 +0100

    Added flash message for invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = Member.find_by(lastName: params[:session][:name])
+    if user && user.password == params[:session][:password]
+      # Log the user in and redirect to the user's show page.
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
 
   def destroy
