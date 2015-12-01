9e3129394d8b5f3ff8cb4e01bf33da9338c00d69
rennersebastian <renner.sebastian@gmail.com>
2015-12-01 16:48:43 +0100

    Updated session helper, added link to login to index page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
   def log_in(member)
     session[:member_id] = member.id
   end
+  
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= Member.find_by(id: session[:member_id])
+  end
+  
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
 end
