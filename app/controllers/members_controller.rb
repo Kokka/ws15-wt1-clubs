bbc7d8744d20a4a54cd80a436f4fa8b872150a52
rennersebastian <renner.sebastian@gmail.com>
2015-12-01 17:06:55 +0100

    Added logout functionality

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -28,6 +28,7 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @member.save
+        log_in @member
         format.html { redirect_to @member, notice: 'Member was successfully created.' }
         format.json { render :show, status: :created, location: @member }
       else
